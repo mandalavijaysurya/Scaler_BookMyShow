4cc023ea629574b7f2caf694f83c746dc7fb3d81
Lynx <mandala.vijay.surya@gmail.com>
2024-07-04 08:12:30 +0530

    Initial setup

---
+++ src/test/java/org/scaler/bookmyshow/BookmyshowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.scaler.bookmyshow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmyshowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
